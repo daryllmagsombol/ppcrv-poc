d181ee7d6ebea6dfa73e2f33c6cf9d66c36ffc42
Daryll Magsombol <daryllmagsombol@Darylls-MacBook-Air.local>
2026-07-16 16:37:56 +0800

    feat(api): register RedisModule in AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ResultsModule } from './modules/results/results.module';
 import { ScanModule } from './modules/scan/scan.module';
 import { AnalyticsModule } from './modules/analytics/analytics.module';
+import { RedisModule } from './modules/redis/redis.module';
 
 @Module({
-  imports: [ResultsModule, ScanModule, AnalyticsModule],
+  imports: [ResultsModule, ScanModule, AnalyticsModule, RedisModule],
 })
 export class AppModule {}
